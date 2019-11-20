d3f707ddaca54946ba6ab57fba4f3805e7f8d538
Valerie Foster <fosterv2@outlook.com>
2019-11-20 23:05:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input.to_i - 1
+  input_to_index(input)
   if index.between?(0, 8)
     move(board, index)
   else
@@ -15,6 +15,10 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
 def valid_move?(board, index)

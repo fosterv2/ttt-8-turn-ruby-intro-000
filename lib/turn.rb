6c6f2a06e8b6bd1701e65985011e86e255e1e0a3
Valerie Foster <fosterv2@outlook.com>
2019-11-20 22:33:18 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def turn
+  
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if !position_taken(board, index) && index.between(0, 8)
+    true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" | board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def move(board, index, token = "X")
+  if valid_move?(board, index)
+    board[index] = token
+  end
+end
